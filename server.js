6f5fd34e291a5074f6207995faa5ecc3bad87695
nick.s <nick.skelin@gmail.com>
2018-10-05 15:11:15 -0700

    Installed handle bars, added a basic navbar template, added my page to the server under temporary path.

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const port = process.env.port || 8080;
 const express = require('express');
+const hbs = require('hbs')
 
 const app = express();
 
+app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials')
+app.use(express.static(__dirname + '/css'))
 
+
+
+app.get('/', (req, res) => {
+	res.render('provider page.hbs')
+})
 
 app.listen(process.env.PORT || 8080, () => {
     console.log(`server up on port ${port}`)
