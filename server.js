13eddbdfb6870411fb8b88efd5a1be1bc537aa7e
nick.s <nick.skelin@gmail.com>
2018-10-11 21:36:18 -0700

    Finished basic css/html implementation of provider page

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 const port = process.env.port || 8080;
 const express = require('express');
-const hbs = require('hbs')
+const hbs = require('hbs');
 
 const app = express();
 
-app.set('view engine', 'hbs')
-hbs.registerPartials(__dirname + '/views/partials')
-app.use(express.static(__dirname + '/css'))
-
-
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+app.use(express.static(__dirname + '/css'));
 
 app.get('/provider', (req, res) => {
 	res.render('provider page.hbs')
-})
+});
 
 app.get('/provider_login', (req, res) => {
 	res.render('login.hbs')
-})
+});
 
 app.get('/dashboard', (req, res) => {
 	res.render('dashboard.hbs')
-})
+});
 
 app.listen(process.env.PORT || 8080, () => {
     console.log(`server up on port ${port}`)
