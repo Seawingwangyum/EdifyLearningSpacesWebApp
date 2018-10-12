1e21f9aea50d21efd7e4dc753cdb2238d3eb9638
Manalune <erkrezm@gmail.com>
2018-10-11 23:03:50 -0700

    ad and account creation pages

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('view engine', 'hbs')
 hbs.registerPartials(__dirname + '/views/partials')
 app.use(express.static(__dirname + '/css'))
 const fs = require('fs');
-const hbs = require('hbs');
+//const hbs = require('hbs');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -25,6 +25,14 @@ app.get('/provider_login', (req, res) => {
 
 app.get('/dashboard', (req, res) => {
 	res.render('dashboard.hbs')
+})
+
+app.get('/account_creation', (req, res) => {
+	res.render('account_creation.hbs')
+})
+
+app.get('/ad_page', (req, res) => {
+	res.render('ad_page.hbs')
 })
 
 app.listen(process.env.PORT || 8080, () => {
