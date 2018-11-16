7cf7ad85eddca7f3764ca8924e024d4f36bd5010
Manalune <erkrezm@gmail.com>
2018-11-16 06:31:41 -0800

    added forgot password and some of its functionality, modified providers quiz

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
+//forgot_pass
+var mysql = require('mysql');
+var nodemailer = require('nodemailer');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var bcrypt = require('bcrypt-nodejs');
+var async = require('async');
+var crypto = require('crypto');
+
+
+
 const port = process.env.port || 8080;
 const express = require('express');
+//forgot_pass
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+
 const hbs = require('hbs');
 const fs = require('fs');
 const session = require('client-sessions');
@@ -19,6 +35,9 @@ app.use(express.static(__dirname + '/fonts'));
 
 app.use(express.static(__dirname + '/node_modules/sweetalert/dist'))
 app.use(express.static(__dirname + '/node_modules/sweetalert2/dist'))
+//forgot_pass
+app.use(logger('dev'));
+app.use(cookieParser());
 
 // bodyparser setup
 var bodyParser = require('body-parser')
@@ -121,6 +140,10 @@ app.get('/provider_edit', (req, res) => {
 
 app.get('/landing_page', (req, res) => {
 	res.render('landing_page.hbs')
+});
+
+app.get('/pass_forgot', (req, res) => {
+    res.render('pass_forgot.hbs')
 });
 
 app.get('/edify_quiz', (req, res) => {
@@ -250,4 +273,60 @@ app.get('/quizresults', (request, response) => {
 
 app.listen(process.env.PORT || 8080, () => {
     console.log(`server up on port ${port}`)
+});
+
+
+
+//forgot_pass
+app.post('/pass_forgot', function(req, res, next) {
+  async.waterfall([
+    function(done) {
+      crypto.randomBytes(20, function(err, buf) {
+        var token = buf.toString('hex');
+        done(err, token);
+      });
+    },
+/**
+    function(token, done) {
+      User.findOne({ email: req.body.email }, function(err, user) {
+        if (!user) {
+          req.flash('error', 'No account with that email address exists.');
+          return res.redirect('/pass_forgot');
+        }
+
+        user.resetPasswordToken = token;
+        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+        user.save(function(err) {
+          done(err, token, user);
+        });
+      });
+    },
+**/
+    function(token, user, done) {
+      var smtpTransport = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          user: 'edifyprovidersreset@gmail.com',
+          pass: 'EdifySpaces'
+        }
+      });
+      var mailOptions = {
+        to: 'edifyprovidersreset@gmail.com',
+        from: 'edifyprovidersreset@gmail.com',
+        subject: 'Node.js Password Reset',
+        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your Edify Providers account.\n\n' +
+          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+          'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+      };
+      smtpTransport.sendMail(mailOptions, function(err) {
+        console.log('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+        //done(err, 'done');
+      });
+    }
+  ], function(err) {
+    if (err) return next(err);
+    res.redirect('/pass_forgot');
+  });
 });
