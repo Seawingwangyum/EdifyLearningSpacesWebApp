4cc8182642965dc9a91298eeeb0c4b99032e8aec
Vivian Nguyen <viviannguyen.bee@gmail.com>
2018-11-22 21:04:46 -0800

    mysql pool

--- test_mqsql.js
+++ test_mqsql.js
@@ -1,20 +1,18 @@
+require('dotenv').config();
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "edify"
+var con = mysql.createPool({
+  host: process.env.DBHOST,
+  user: process.env.DBUSER,
+  password: process.env.DBPASSWORD,
+  database: process.env.DBNAME,
+  port: process.env.DBPORT
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
 
-  con.query("INSERT INTO user(first_name, last_name, password, email, location, is_admin) values ('fred', 'jeff', 'password', 'fred@jeff.com', 'Surrey', '0')", function (err, result) {
-    if (err) throw err;
-    console.log("Insert Successful");
-	});
+con.query("INSERT INTO user(first_name, last_name, password, email, location, is_admin) values ('fred', 'jeff', 'password', 'fred@jeff.com', 'Surrey', '0')", function (err, result) {
+  if (err) throw err;
+  console.log("Insert Successful");
 });
 
 
